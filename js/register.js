1baf22c06fd1746385d858cb6085f17712543a00
JosemaRam <ramirezdelafuente.jm@gmail.com>
2022-02-22 22:52:43 -0300

    profesor, no pude con el js, perdón por la demora

--- js/register.js
+++ js/register.js
@@ -2,3 +2,12 @@ window.addEventListener("DOMContentLoaded", (loadEvent) => {
     $("header").load("includes/header.html");
     $("footer").load("includes/footer.html");
 });
+
+document.getElementById("rut").addEventListener("blur" , (e) => {
+    const rut = parseInt( document.getElementById("rut").value );
+});
+
+    if( isNaN(rut)  )   
+    
+    if( rut > 100000000 &&  rut <  999999999);
+    
